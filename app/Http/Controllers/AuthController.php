096b24f99235a005c959bcd3bf03e48e47322c0c
Mohammed Alnuaimi <mnuaimi@cerebra.sa>
2024-09-27 00:53:58 +0300

    episode_02

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     Use ApiResponses;
-    public function Login(){
-        return  $this->ok('Hello,Login');
+    public function Login(ApiLoginRequest $request){
+        return  $this->ok($request->get('email'));
+    }
+    public function register(){
+        return $this->ok('register');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login',[AuthController::class,'login']);
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
